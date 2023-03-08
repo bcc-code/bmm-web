3a142c642b9d0e15359e06db2411da99a7a54d32
Karsten Küpper <karsten.kuepper@bcc.no>
2023-03-08 18:06:58 +0100

    use URL for url manipulation

--- src/utils/filters.ts
+++ src/utils/filters.ts
@@ -1,7 +1,11 @@
 export default {
-    authorizedUrl(url) {
-        if (url && !url.includes("auth"))
-            return `${url}&auth=${import.meta.env.VITE_BMM_TOKEN}`;
-        return url;
+    authorizedUrl(urlString) {
+        if (!urlString) {
+            return urlString;
+        }
+
+        var url = new URL(urlString);
+        url.searchParams.set("auth", import.meta.env.VITE_BMM_TOKEN);
+        return url.toString();
     }
 }
