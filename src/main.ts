563a2c6a914175247bacf171e5c54898b1e7bfae
Gerard <gerardlamo@gmail.com>
2023-03-08 21:35:03 +0100

    Added cacheLocation: 'localstorage'

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,11 @@ app.use(
   createAuth0({
     domain: import.meta.env.VITE_AUTH_URL,
     clientId: import.meta.env.VITE_CLIENT_ID,
+    cacheLocation: 'localstorage',
     authorizationParams: {
       redirect_uri: window.location.origin,
-      audeince: 'https://bmm-api.brunstad.org'
-    }
+      audeince: 'https://bmm-api.brunstad.org',
+    },
   })
 );
 
