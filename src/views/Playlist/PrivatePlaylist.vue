59ada227ad031c447e4bad822b1bcf35f7c7cb99
Sigve Hansen <sighan12@gmail.com>
2023-03-23 21:32:28 +0100

    style: prettier

--- src/views/Playlist/PrivatePlaylist.vue
+++ src/views/Playlist/PrivatePlaylist.vue
@@ -1,45 +1,42 @@
 <script lang="ts" setup>
-import { Configuration, GetTrackCollectionModel, TrackCollectionApi } from '@bcc-code/bmm-sdk-fetch';
-import { computed, ref } from 'vue';
+import {
+  Configuration,
+  GetTrackCollectionModel,
+  TrackCollectionApi,
+} from '@bcc-code/bmm-sdk-fetch'
+import { ref } from 'vue'
 
 const props = defineProps<{
-	id: string
+  id: string
 }>()
 
 const trackCollection = ref<GetTrackCollectionModel>({})
-const sortedTracks = computed(() => {
-	const { tracks } = trackCollection.value
-	return tracks?.sort((a, b) => {
-		if (!b.order || !a.order) return 0
-		return b.order - a.order
-	})
-})
 
 new TrackCollectionApi(
-	new Configuration({
-		basePath: import.meta.env.VITE_API_URL,
-		headers: {
-			Authorization: `Bearer ${localStorage.getItem("token")}`,
-			"Accept-Language": "nb,en,zxx",
-		},
-	})
+  new Configuration({
+    basePath: import.meta.env.VITE_API_URL,
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+      'Accept-Language': 'nb,en,zxx',
+    },
+  })
 )
-	.trackCollectionIdGet({
-		id: Number(props.id)
-	})
-	.then(collection => {
-		trackCollection.value = collection
-	})
-	.catch(() => { })
+  .trackCollectionIdGet({
+    id: Number(props.id),
+  })
+  .then(collection => {
+    trackCollection.value = collection
+  })
+  .catch(() => {})
 </script>
 
 <template>
-	<h1>{{ trackCollection?.name }}</h1>
-	<table>
-		<ol class="list-decimal list-inside">
-			<li v-for="track in sortedTracks" :key="track.id">
-				{{ track.title }}
-			</li>
-		</ol>
-	</table>
+  <h1>{{ trackCollection?.name }}</h1>
+  <table>
+    <ol class="list-decimal list-inside">
+      <li v-for="track in trackCollection.tracks" :key="track.id">
+        {{ track.title }}
+      </li>
+    </ol>
+  </table>
 </template>
