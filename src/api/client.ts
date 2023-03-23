b2b30d47fba01bd7cc5f53b76e81a06c2594f8e4
Fredrik Vedvik <fredrik@vedvik.tech>
2023-03-23 23:48:49 +0100

    fix (broken authentication)

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,13 +1,15 @@
-import { useAuth0 } from "@auth0/auth0-vue";
+import auth0 from "@/auth0";
 import { BaseAPI, Configuration, Middleware } from "@bcc-code/bmm-sdk-fetch";
 
 type TokenFactory = () => Promise<string | null>;
 
 export class Client {
   constructor(tokenFactory: TokenFactory) {
+    
     this.middleware = {
       pre: async (ctx) => {
         const token = await tokenFactory();
+
         const headers = new Headers(ctx.init.headers);
         if (token) {
           headers.set("Authorization", `Bearer ${token}`);
@@ -25,6 +27,10 @@ export class Client {
   public use<T extends BaseAPI>(Api: new (configuration: Configuration) => T) {
     return new Api(
       new Configuration({
+        basePath: import.meta.env.VITE_API_URL,
+        headers: {
+          "Accept-Language": "nb,en,zxx",
+        },
         middleware: [this.middleware],
       })
     );
@@ -32,7 +38,7 @@ export class Client {
 }
 
 export default new Client(async () => {
-  const { getAccessTokenSilently, isAuthenticated } = useAuth0();
+  const { getAccessTokenSilently, isAuthenticated } = auth0;
   if (!isAuthenticated.value) {
     return null;
   }
