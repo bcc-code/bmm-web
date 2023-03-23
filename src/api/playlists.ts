8ecf38bcb04ba48353fabdedf39f456d7d350567
Fredrik Vedvik <fredrik@vedvik.tech>
2023-03-23 16:31:35 +0100

    formatting

--- src/api/playlists.ts
+++ src/api/playlists.ts
@@ -1,13 +1,7 @@
-import {
-  PlaylistApi,
-  PlaylistModel,
-} from "@bcc-code/bmm-sdk-fetch";
+import { PlaylistApi, PlaylistModel } from "@bcc-code/bmm-sdk-fetch";
 import client from "./client";
 
-export const list = async () => {
-  return client.attach(PlaylistApi).playlistGet();
-};
+export const list = async () => client.attach(PlaylistApi).playlistGet();
 
-export const get = async (id: NonNullable<PlaylistModel["id"]>) => {
-  return client.attach(PlaylistApi).playlistIdGet({ id });
-};
+export const get = async (id: NonNullable<PlaylistModel["id"]>) =>
+  client.attach(PlaylistApi).playlistIdGet({ id });
