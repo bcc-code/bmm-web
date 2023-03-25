a238583563bdcc32d45c73f17a9c7d0e90a59ce7
Sigve Hansen <sighan12@gmail.com>
2023-03-25 21:16:58 +0100

    chore: remove custom css & define font in tailwind config

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useAuth0 } from "@auth0/auth0-vue";
 import { watch } from "vue";
+import Toolbar from "./components/AppToolbar.vue";
 
 // logout
 const { isLoading, loginWithRedirect, isAuthenticated } = useAuth0();
@@ -10,34 +11,34 @@ watch(isLoading, async (loading) => {
     await loginWithRedirect();
   }
 });
+
+const navLinks = [
+  { to: "/", name: "Home" },
+  { to: "/browse", name: "Browse" },
+  { to: "/search", name: "Search" },
+];
 </script>
 
 <template>
   <div class="flex">
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/browse">Browse</router-link>
-      <router-link to="/search">Search</router-link>
+    <nav
+      class="bg-gray-50 border-r border-gray-100 p-4 sticky top-0 h-screen min-w-[200px]"
+    >
+      <router-link
+        v-for="link in navLinks"
+        :key="link.name"
+        :to="link.to"
+        class="p-2 rounded-lg flex"
+        active-class="bg-lime-400"
+      >
+        {{ link.name }}
+      </router-link>
     </nav>
-    <main class="overflow-scroll p-5">
-      <router-view v-if="isAuthenticated" />
-    </main>
+    <div>
+      <Toolbar />
+      <main class="p-5 grow">
+        <router-view v-if="isAuthenticated" />
+      </main>
+    </div>
   </div>
 </template>
-
-<style scoped>
-nav a {
-  display: block;
-  color: black;
-  font-style: normal;
-  font-weight: 700;
-  font-size: 18px;
-  line-height: 24px;
-  padding: 12px;
-}
-
-nav a.router-link-active {
-  background: #a4e16a;
-  border-radius: 12px;
-}
-</style>
