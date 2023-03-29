0f99c756118f096545d56171b627aabf6d64d105
Simon Schick <simonsimcity@gmail.com>
2023-03-29 10:28:47 +0200

    Refactored api configuration into vue-plugin

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import App from "./App.vue";
 import router from "./router";
 import Store from "./store";
 import auth0 from "./auth0";
+import bmmApi from "./plugins/bmm-api";
 
 const app = createApp(App);
 
 app.use(router);
 
 app.use(auth0);
+
+app.use(bmmApi);
 
 app.provide(
   "store",
