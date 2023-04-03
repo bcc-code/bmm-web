b297825642904c7c18e1ce7c31c15752f7c3bfd0
Simon Schick <simonsimcity@gmail.com>
2023-04-03 15:26:02 +0200

    Report unused and missing keys on eslint as warning
    TypeScript does not anymore report missing keys as error (as they aren't errors - nothing will fail). With the current setup, language files have to be in the extension and format "json" or "json5", despite the plugin supporting more formats.

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -2,9 +2,7 @@ import { createI18n } from "vue-i18n";
 import en from "../locales/en.json";
 import de from "../locales/de.json";
 
-type MessageSchema = typeof en & typeof de;
-
-const i18n = createI18n<[MessageSchema], "en" | "de">({
+const i18n = createI18n<[{}], "en" | "de">({
   legacy: false,
   locale: "en",
   messages: {
