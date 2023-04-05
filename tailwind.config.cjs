33123bf583be45d06ff4e3db84c3989305fa7b92
Simon Schick <simonsimcity@gmail.com>
2023-04-05 13:35:11 +0200

    Applied linting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      sans: ['Inter', 'Avenir', 'Helvetica', 'Arial', 'sans-serif']
+      sans: ["Inter", "Avenir", "Helvetica", "Arial", "sans-serif"],
     },
     extend: {},
   },
