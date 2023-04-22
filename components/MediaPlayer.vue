edee2c233b457bb60c87c6e4361edd43fdb2f311
Zakhariy Derekh <zakhariy@ownersroom.com>
2023-04-22 17:00:41 +0200

    Changed the media player to operate with track object

--- components/MediaPlayer.vue
+++ components/MediaPlayer.vue
@@ -6,7 +6,7 @@ import {
 } from "~/plugins/3.mediaPlayer";
 
 const { status, play, pause } = inject(MediaPlayerInjectionKey)!;
-const { currentSong } = inject(MediaPlaylistInjectionKey)!;
+const { currentTrack } = inject(MediaPlaylistInjectionKey)!;
 </script>
 <template>
   <div class="fixed bottom-3 right-3 w-[316px] height-5">
@@ -18,8 +18,7 @@ const { currentSong } = inject(MediaPlaylistInjectionKey)!;
     <a href="">🔀</a>
     <a href="">🔁</a>
 
-    <span style="max-width: 100px; overflow: hidden; display: inline-block"
-      >Current track: {{ currentSong }}</span
-    >
-  </div>
+  <span style="max-width: 100px; overflow: hidden; display: inline-block"
+    >Current track: {{ currentTrack }}</span
+  >
 </template>
