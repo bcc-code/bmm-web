5ce466c9f9d297416904bac53d5c01c4b7c68dd2
Karsten Küpper <karsten.kuepper@bcc.no>
2023-04-22 16:23:33 +0200

    fix more linting complaints

--- electron/main.ts
+++ electron/main.ts
@@ -1,7 +1,14 @@
 import { app, BrowserWindow } from "electron";
-app.whenReady().then(() => {
-  new BrowserWindow().loadURL(
-    process.env.VITE_DEV_SERVER_URL ||
-      `file://${__dirname}/../.output/public/index.html`
-  );
-});
+
+app
+  .whenReady()
+  .then(() => {
+    new BrowserWindow().loadURL(
+      process.env.VITE_DEV_SERVER_URL ||
+        // eslint-disable-next-line n/no-path-concat
+        `file://${__dirname}/../.output/public/index.html`
+    );
+  })
+  .catch((error) => {
+    console.error(error);
+  });
