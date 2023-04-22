6abd0dd9f4caef2cf1ae60091bfd7eac01c9e53e
Sigve Hansen <sighan12@gmail.com>
2023-04-22 16:22:04 +0200

    chore: change token file

--- tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import * as colors from "@/assets/design-tokens/raw/color.json";
-import type { Config } from "tailwindcss";
-
-console.log(colors);
-export default <Partial<Config>>{
-  theme: {},
-};
