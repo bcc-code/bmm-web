792c012234cee097ddfa27003f1986e8623fa468
Steffan <steffanhalv@gmail.com>
2023-04-22 15:51:29 +0200

    Remove files from dist-electron

--- dist-electron/main.js
+++
@@ -1,7 +0,0 @@
-var import_electron = require("electron");
-import_electron.app.whenReady().then(() => {
-  new import_electron.BrowserWindow().loadURL(
-    "http://localhost:9001"
-    /*process.env.VITE_DEV_SERVER_URL*/
-  );
-});
