94507dc83a29aa7538986a5ced9eb7d1e7022be8
Bjørnar Hagen <bjornar@datahjelpen.no>
2023-04-23 11:39:51 +0200

    replace media player emojis with icons

--- components/MediaPlayer.vue
+++ components/MediaPlayer.vue
@@ -10,13 +10,33 @@ const { currentSong } = inject(MediaPlaylistInjectionKey)!;
 </script>
 <template>
   <div class="fixed bottom-3 right-3 w-[316px] height-5">
-    <a href="">⏪</a>
-    <a v-if="status !== MediaPlayerStatus.Playing" @click="play()">⏵</a>
-    <a v-if="status === MediaPlayerStatus.Playing" @click="pause()">⏸</a>
-    <a href="">⏩</a>
+    <button class="p-1 bg-slate-100 hover:bg-slate-200">
+      <IconComponent name="icon.previous.track.large" />
+    </button>
+    <button
+      class="p-1 bg-slate-100 hover:bg-slate-200"
+      v-if="status !== MediaPlayerStatus.Playing"
+      @click="play()"
+    >
+      <IconComponent name="icon.play.small" />
+    </button>
+    <button
+      class="p-1 bg-slate-100 hover:bg-slate-200"
+      v-if="status === MediaPlayerStatus.Playing"
+      @click="pause()"
+    >
+      <IconComponent name="icon.pause.small" />
+    </button>
+    <button class="p-1 bg-slate-100 hover:bg-slate-200">
+      <IconComponent name="icon.next.track.large" />
+    </button>
 
-    <a href="">🔀</a>
-    <a href="">🔁</a>
+    <button class="p-1 bg-slate-100 hover:bg-slate-200">
+      <IconComponent name="icon.shuffle" />
+    </button>
+    <button class="p-1 bg-slate-100 hover:bg-slate-200">
+      <IconComponent name="icon.repeat" />
+    </button>
 
     <span style="max-width: 100px; overflow: hidden; display: inline-block"
       >Current track: {{ currentSong }}</span
