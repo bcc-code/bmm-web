94650028a33d4e0f6c6f2f623234e60228a63989
Sigve Hansen <sighan12@gmail.com>
2023-04-24 09:01:08 +0200

    fix: improve mediaplayer design

--- components/MediaPlayer.vue
+++ components/MediaPlayer.vue
@@ -8,33 +8,48 @@ import {
 const { status, play, pause } = inject(MediaPlayerInjectionKey)!;
 const { currentTrack } = inject(MediaPlaylistInjectionKey)!;
 </script>
+
 <template>
-  <div class="fixed bottom-3 right-3 flex w-[400px] p-3 rounded-2xl shadow-xl bg-white-1">
+  <div
+    class="fixed bottom-4 right-4 flex w-[400px] p-3 rounded-2xl shadow-xl bg-white-1 border border-black-1/10"
+  >
     <div class="flex flex-1 gap-3 min-w-0">
       <div class="flex flex-1 gap-3 min-w-0">
-        <div class="shrink-0 rounded-xl bg-background-2 w-[48px] overflow-hidden">
-          <ProtectedImage v-if="currentTrack?.meta?.attachedPicture" :src="currentTrack?.meta?.attachedPicture" />
+        <div
+          class="shrink-0 rounded-md bg-background-2 h-[48px] aspect-square overflow-hidden"
+        >
+          <ProtectedImage
+            v-if="currentTrack?.meta?.attachedPicture"
+            :src="currentTrack?.meta?.attachedPicture"
+          />
         </div>
         <div class="flex flex-col min-w-0">
-          <h3 class="font-semibold text-lg truncate">
+          <h3
+            class="font-semibold text-lg truncate leading-tight"
+            :title="currentTrack?.title || ''"
+          >
             {{ currentTrack?.title }}
           </h3>
-          <span v-if="currentTrack?.meta?.artist" class="text-label-2 text-base truncate">
+          <span
+            v-if="currentTrack?.meta?.artist"
+            class="text-label-2 text-base truncate leading-snug"
+            :title="currentTrack?.meta?.artist"
+          >
             {{ currentTrack.meta?.artist }}
           </span>
         </div>
       </div>
       <div>
-        <a v-if="status === MediaPlayerStatus.Playing" @click="pause()">
-          <span class="flex aspect-square w-[48px] justify-center align-middle">
+        <button v-if="status === MediaPlayerStatus.Playing" @click="pause()">
+          <span class="flex aspect-square w-12 justify-center align-middle">
             <IconComponent name="icon.pause.large" class="text-3xl" />
           </span>
-        </a>
-        <a v-if="status !== MediaPlayerStatus.Playing" @click="play()">
-          <span class="flex aspect-square w-[48px] justify-center align-middle">
+        </button>
+        <button v-if="status !== MediaPlayerStatus.Playing" @click="play()">
+          <span class="flex aspect-square w-12 justify-center align-middle">
             <IconComponent name="play" class="text-3xl" />
           </span>
-        </a>
+        </button>
       </div>
     </div>
   </div>
