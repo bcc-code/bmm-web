ae3aa868096dc826eb2c64f67770f7f2b424d568
Gerard <gerardlamo@gmail.com>
2023-05-02 20:57:32 +0200

    Add queue support to mediaPlayer Add shuffle to easy test queue Add next and previous

--- pages/playlist/curated/[id].vue
+++ pages/playlist/curated/[id].vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { MediaPlaylistInjectionKey } from '~/plugins/3.mediaPlayer';
+
 const { t } = useI18n();
 toolbarTitleStore().setReactiveToolbarTitle(() => t("nav.playlist"));
 
@@ -7,6 +9,18 @@ const playlistId = Number(id);
 
 const { data: playlist } = usePlaylist({ id: playlistId });
 const { data: tracks, pending } = usePlaylistTracks({ id: playlistId });
+
+const { setCurrentTrack, addTrackToQueue } = inject(MediaPlaylistInjectionKey)!;
+
+function shuffle() {
+  // shuffle tracks and add them to the queue
+  const shuffledTracks = tracks.value?.filter((track) => true);
+  if (shuffledTracks && shuffledTracks.length > 0) {
+    shuffledTracks?.sort(() => Math.random() - 0.5);
+    setCurrentTrack(shuffledTracks.pop()!);
+    shuffledTracks.forEach((track) => addTrackToQueue(track));
+  }
+}
 
 onBeforeMount(() => {
   useHead({
@@ -19,28 +33,20 @@ onBeforeMount(() => {
   <div class="grid grid-cols-12" v-if="playlist">
     <div class="col-start-3 col-span-8">
       <header class="flex gap-6 mb-12">
-        <ProtectedImage
-          v-if="playlist.cover"
-          :src="playlist.cover"
-          alt=""
-          class="aspect-square rounded-2xl bg-slate-100 w-[300px]"
-        />
+        <ProtectedImage v-if="playlist.cover" :src="playlist.cover" alt=""
+          class="aspect-square rounded-2xl bg-slate-100 w-[300px]" />
         <div class="p-6 flex flex-col justify-between">
           <div>
             <PageHeading>{{ playlist.title }}</PageHeading>
             <p v-if="tracks">{{ tracks.length }} tracks</p>
           </div>
           <div class="flex gap-2">
-            <ButtonStyled intent="primary"> Shuffle </ButtonStyled>
+            <ButtonStyled intent="primary" @click="shuffle"> Shuffle </ButtonStyled>
             <ButtonStyled>Follow</ButtonStyled>
           </div>
         </div>
       </header>
-      <TrackList
-        :skeleton-count="10"
-        :show-skeleton="pending"
-        :tracks="tracks"
-      />
+      <TrackList :skeleton-count="10" :show-skeleton="pending" :tracks="tracks" />
     </div>
   </div>
 </template>
