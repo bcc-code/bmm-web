c2b094ddbd8ff3ee85114e973bf4336330779b08
Simon Schick <simonsimcity@gmail.com>
2023-05-10 15:35:09 +0200

    Fixed eslint error in prettier-config

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  // eslint-disable-next-line global-require
+  // eslint-disable-next-line import/no-extraneous-dependencies, global-require
   plugins: [require("prettier-plugin-tailwindcss")],
 };
