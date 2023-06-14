b1a4d6f260d22465c7b84b8e502181eef2b7116a
Sigve Hansen <sighan12@gmail.com>
2023-06-14 21:22:47 +0200

    chore: move language selector to toolbar

--- components/AppToolbar.vue
+++ components/AppToolbar.vue
@@ -1,9 +1,10 @@
 <template>
   <header
-    class="sticky top-0 border-b border-label-separator bg-white-2 px-6 py-4 backdrop-blur-md"
+    class="sticky top-0 z-10 flex justify-between border-b border-label-separator bg-white-2 px-6 py-4 backdrop-blur-md"
   >
     <strong v-if="toolbarTitleStore().toolbarTitle" class="text-label-1">
       {{ toolbarTitleStore().toolbarTitle }}
     </strong>
+    <ChangeLocale class="mx-4" />
   </header>
 </template>
