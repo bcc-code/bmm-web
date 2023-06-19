d6160f5938c8ce4f04297b4413100fd235ddbadb
Simon Schick <simonsimcity@gmail.com>
2023-06-19 16:29:19 +0200

    Electron: Refactored management of windows
    For mac the app doesn't crash anymore if the last window is closed and the user clicks on a link, but reopens a window with the given url.

--- electron/main.ts
+++ electron/main.ts
@@ -1,8 +1,12 @@
 import { app, protocol, shell, dialog, BrowserWindow, net } from "electron";
 import * as path from "path";
+import * as fs from "fs/promises";
 
 const PRODUCTION_APP_PROTOCOL = "bmm";
 const PRODUCTION_APP_PATH = path.join(__dirname, "..", ".output", "public");
+const defaultUrl = `${PRODUCTION_APP_PROTOCOL}://bmm.brunstad.org`;
+let initUrl = defaultUrl;
+let appReadyHasRun = false;
 
 app.setAsDefaultProtocolClient(PRODUCTION_APP_PROTOCOL);
 protocol.registerSchemesAsPrivileged([
@@ -31,101 +35,138 @@ if (!gotTheLock) {
   app.quit();
 }
 
-app
-  .whenReady()
-  .then(() => {
-    if (process.env.VITE_DEV_SERVER_URL) {
-      protocol.registerStreamProtocol(
-        PRODUCTION_APP_PROTOCOL,
-        (request, callback) => {
-          net
-            .request({
-              method: request.method,
-              url: `${process.env.VITE_DEV_SERVER_URL}${removeUrlOrigin(
-                request.url
-              )}`,
-            })
-            .on("response", (response) => {
-              callback(response);
-            })
-            .end();
-        }
-      );
-    } else {
-      protocol.registerFileProtocol(
-        PRODUCTION_APP_PROTOCOL,
-        (request, callback) => {
-          const relativePath = path.normalize(new URL(request.url).pathname);
-          const absolutePath = path.join(
-            PRODUCTION_APP_PATH,
-            relativePath !== path.sep ? relativePath : "index.html"
-          );
+let window: BrowserWindow | undefined;
+const openWindow = (url: string) => {
+  if (!appReadyHasRun) {
+    initUrl = url;
+    return;
+  }
 
-          // eslint-disable-next-line n/no-callback-literal, promise/no-callback-in-promise
-          callback({ path: absolutePath });
-        }
-      );
+  window = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  window.webContents.on("will-navigate", (e, _url) => {
+    // Some links from the API have the fixed domain `bmm.brunstad.org` on the `http(s)` protocol. Use our router instead of navigating (which means reloading the "app").
+    if (/^https?:\/\/bmm\.brunstad\.org\//.test(_url)) {
+      e.preventDefault();
+      navigateToUri(window!, removeUrlOrigin(_url));
+      return;
     }
 
-    const window = new BrowserWindow({
-      webPreferences: {
-        preload: path.join(__dirname, "preload.js"),
-      },
-    });
-
-    window.webContents.on("will-navigate", (e, url) => {
-      // Some links from the API have the fixed domain `bmm.brunstad.org` on the `http(s)` protocol. Use our router instead of navigating (which means reloading the "app").
-      if (/^https?:\/\/bmm\.brunstad\.org\//.test(url)) {
-        e.preventDefault();
-        navigateToUri(window, removeUrlOrigin(url));
-        return;
-      }
-
-      // Introduced to handle login-process in the default browser. A user usually has his login credentials already saved there.
-      if (!url.startsWith(`${PRODUCTION_APP_PROTOCOL}://`)) {
-        e.preventDefault();
-        shell.openExternal(url).catch((error) => {
-          dialog.showErrorBox(url, `${error}`);
-        });
-      }
-    });
-
-    // Event is triggered when another program opens a `bmm://` link.
-    app.on("open-url", (_, url) => {
-      if (/^bmm:\/\//.test(url)) {
-        navigateToUri(window, removeUrlOrigin(url));
-      } else {
-        window.loadURL(url);
-      }
-    });
-
-    app.on("second-instance", (_, commandLine) => {
-      // Someone tried to run a second instance, we should focus our window.
-      if (window) {
-        if (window.isMinimized()) window.restore();
-        window.focus();
-      }
-
-      // What else would be the first argument ..?
-      const url = commandLine.pop() || "";
-
-      if (/^bmm:\/\//.test(url)) {
-        navigateToUri(window, removeUrlOrigin(url));
-      } else {
-        window.loadURL(url);
-      }
-    });
-
-    return window.loadURL(`${PRODUCTION_APP_PROTOCOL}://bmm.brunstad.org`);
-  })
-  .catch((error) => {
-    // eslint-disable-next-line promise/catch-or-return
-    import("log-symbols").then((t) => console.error(t.default.error, error));
+    // Introduced to handle login-process in the default browser. A user usually has his login credentials already saved there.
+    if (!_url.startsWith(`${PRODUCTION_APP_PROTOCOL}://`)) {
+      e.preventDefault();
+      shell.openExternal(_url).catch((error) => {
+        dialog.showErrorBox(_url, `${error}`);
+      });
+    }
   });
+
+  window.loadURL(url).catch((error) => {
+    dialog.showErrorBox(url, `${error}`);
+  });
+
+  window.on("closed", () => {
+    window = undefined;
+  });
+};
+
+// eslint-disable-next-line promise/catch-or-return
+app.whenReady().then(() => {
+  if (process.env.VITE_DEV_SERVER_URL) {
+    protocol.registerStreamProtocol(
+      PRODUCTION_APP_PROTOCOL,
+      (request, callback) => {
+        net
+          .request({
+            method: request.method,
+            url: `${process.env.VITE_DEV_SERVER_URL}${removeUrlOrigin(
+              request.url
+            )}`,
+          })
+          .on("response", (response) => {
+            callback(response);
+          })
+          .end();
+      }
+    );
+  } else {
+    protocol.registerFileProtocol(
+      PRODUCTION_APP_PROTOCOL,
+      (request, callback) => {
+        const relativePath = path.normalize(new URL(request.url).pathname);
+        const absolutePath = path.join(
+          PRODUCTION_APP_PATH,
+          relativePath !== path.sep ? relativePath : "index.html"
+        );
+
+        // Can the file be accessed? If yes, serve it. If not, it's most likely a route which we should resolve by opening index.html.
+        fs.access(absolutePath)
+          .then(() => fs.lstat(absolutePath))
+          .then((stat) => {
+            if (stat.isFile()) {
+              // eslint-disable-next-line n/no-callback-literal
+              callback({ path: absolutePath });
+            } else {
+              // eslint-disable-next-line no-throw-literal, @typescript-eslint/no-throw-literal
+              throw undefined;
+            }
+          })
+          .catch(() => {
+            // eslint-disable-next-line n/no-callback-literal
+            callback({ path: path.join(PRODUCTION_APP_PATH, "index.html") });
+          });
+      }
+    );
+  }
+
+  appReadyHasRun = true;
+  openWindow(initUrl);
+});
+
+// Event is triggered when another program opens a `bmm://` link.
+app.on("open-url", (_, url) => {
+  if (!window) {
+    openWindow(url);
+  } else if (/^bmm:\/\//.test(url)) {
+    navigateToUri(window, removeUrlOrigin(url));
+  } else {
+    window.loadURL(url);
+  }
+});
+
+app.on("second-instance", (_, commandLine) => {
+  // What else would be the first argument ..?
+  const url = commandLine.pop() || "";
+
+  // Someone tried to run a second instance, we should focus our window.
+  if (window) {
+    if (window.isMinimized()) window.restore();
+    window.focus();
+
+    if (/^bmm:\/\//.test(url)) {
+      navigateToUri(window, removeUrlOrigin(url));
+    } else {
+      window.loadURL(url);
+    }
+  } else {
+    openWindow(url);
+  }
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
+});
+
+// On MacOS, if the icon in the dock is tapped ...
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    openWindow(defaultUrl);
+  }
 });
