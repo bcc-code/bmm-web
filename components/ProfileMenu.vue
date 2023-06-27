010b20498f5e0d911be40b181902d08a36a2707a
Simon Schick <simonsimcity@gmail.com>
2023-06-27 12:54:54 +0200

    Reduce and clear up classes

--- components/ProfileMenu.vue
+++ components/ProfileMenu.vue
@@ -4,18 +4,14 @@ import { Menu, MenuButton, MenuItems, MenuItem, Switch } from "@headlessui/vue";
 const enabled = ref(false);
 </script>
 <template>
-  <div class="text-right">
-    <Menu as="div" class="relative inline-block text-left">
+  <div>
+    <Menu as="div" class="relative">
       <div>
         <MenuButton
-          class="text-white focus-visible:ring-white inline-flex w-full justify-center rounded-md bg-opacity-20 px-4 font-bold text-label-1 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75"
+          class="flex items-center font-bold text-label-1 focus:outline-none"
         >
           Profile
-          <IconComponent
-            name="nav.profile"
-            class="-mr-1 ml-1 text-xl font-bold group-hover:text-4xl"
-            aria-hidden="true"
-          />
+          <NuxtIcon name="nav.profile" class="ml-1 text-xl" />
         </MenuButton>
       </div>
 
@@ -28,15 +24,13 @@ const enabled = ref(false);
         leave-to-class="transform scale-95 opacity-0"
       >
         <MenuItems
-          class="divide-gray-100 ring-black absolute right-0 mt-2 w-56 origin-top-right divide-y rounded-md bg-white-1 shadow-lg ring-1 ring-opacity-5 focus:outline-none"
+          class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-label-separator rounded-md bg-white-1 text-sm shadow-lg ring-1 ring-opacity-5 focus:outline-none dark:divide-label-dark-separator"
         >
-          <div class="list-none px-1 py-1">
-            <MenuItem v-slot="{ active }" as="div" class="flex justify-between">
+          <div class="px-1 py-1">
+            <MenuItem v-slot="{ active }" as="div" class="flex items-center">
               <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center justify-between rounded-md px-2 py-2 text-sm',
-                ]"
+                :class="active ? 'bg-violet-500 text-white' : 'text-gray-900'"
+                class="w-full px-2 py-2 text-left"
               >
                 Auto Play
               </button>
@@ -45,7 +39,6 @@ const enabled = ref(false);
                 :class="enabled ? 'bg-teal-[500]' : 'bg-teal-[500]'"
                 class="border-transparent focus-visible:ring-white relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75"
               >
-                <!-- <span class="sr-only">Use setting</span> -->
                 <span
                   aria-hidden="true"
                   :class="enabled ? 'translate-x-9' : 'translate-x-0'"
@@ -55,74 +48,60 @@ const enabled = ref(false);
             </MenuItem>
             <MenuItem v-slot="{ active }" as="div">
               <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
+                :class="active ? 'bg-violet-500 text-white' : 'text-gray-900'"
+                class="w-full px-2 py-2 text-left"
               >
                 Theme
               </button>
             </MenuItem>
             <MenuItem v-slot="{ active }" as="div">
               <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
+                :class="active ? 'bg-violet-500 text-white' : 'text-gray-900'"
+                class="w-full px-2 py-2 text-left"
               >
                 Language
               </button>
             </MenuItem>
             <MenuItem v-slot="{ active }" as="div">
               <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
+                :class="active ? 'bg-violet-500 text-white' : 'text-gray-900'"
+                class="w-full px-2 py-2 text-left"
               >
                 Content Language
               </button>
             </MenuItem>
           </div>
-          <div class="list-none px-1 py-1">
+          <div class="px-1 py-1">
             <MenuItem v-slot="{ active }" as="div">
               <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
+                :class="active ? 'bg-violet-500 text-white' : 'text-gray-900'"
+                class="w-full px-2 py-2 text-left"
               >
                 Feedback
               </button>
             </MenuItem>
             <MenuItem v-slot="{ active }" as="div">
               <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
+                :class="active ? 'bg-violet-500 text-white' : 'text-gray-900'"
+                class="w-full px-2 py-2 text-left"
               >
                 Contact
               </button>
             </MenuItem>
             <MenuItem v-slot="{ active }" as="div">
               <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
+                :class="active ? 'bg-violet-500 text-white' : 'text-gray-900'"
+                class="w-full px-2 py-2 text-left"
               >
                 About BMM
               </button>
             </MenuItem>
           </div>
-          <div class="list-none px-1 py-1">
+          <div class="px-1 py-1">
             <MenuItem v-slot="{ active }" as="div">
               <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                ]"
+                :class="active ? 'bg-violet-500 text-white' : 'text-gray-900'"
+                class="w-full px-2 py-2 text-left"
               >
                 Sign Out
               </button>
