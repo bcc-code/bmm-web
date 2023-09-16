048a8e1d9e47ce28b93d5d189afcf5e5a972f0a9
Joachim Velzel <joachim.velzel@treeva.de>
2023-09-16 18:24:35 +0200

    65 - Added closed and open player logic

--- components/MediaPlayer.vue
+++ components/MediaPlayer.vue
@@ -1,38 +1,32 @@
 <script setup lang="ts">
 import { MediaPlayerStatus } from "~/plugins/mediaPlayer/mediaPlayer";
 
+const open = ref(false);
+
 const {
   status,
   play,
   pause,
-  hasNext,
-  next,
-  previous,
-  hasPrevious,
   currentTrack,
   currentPosition,
   currentTrackDuration,
-  isLoading,
   queue,
 } = useNuxtApp().$mediaPlayer;
 </script>
 
 <template>
-  <div
-    class="fixed bottom-4 right-4 flex w-[400px] flex-col rounded-2xl border border-black-1/10 bg-white-1 p-3 shadow-xl dark:border-white-1/10 dark:bg-black-1"
+  <transition
+    enter-active-class="transition-all duration-200 ease-out"
+    enter-from-class="opacity-0 translate-y-2"
+    leave-active-class="transition-all duration-200 ease-out"
+    leave-to-class="opacity-0 translate-y-2"
   >
-    <div class="flex min-w-0 flex-1 gap-3">
-      <div class="group select-none">
-        <button v-if="hasPrevious" @click.stop="previous()">
-          <span class="flex aspect-square w-12 justify-center align-middle">
-            <NuxtIcon
-              name="icon.previous.track.large"
-              class="text-3xl group-hover:text-4xl"
-            />
-          </span>
-        </button>
-      </div>
-      <div class="flex min-w-0 flex-1 gap-3">
+    <div
+      class="absolute bottom-5 right-5 flex flex-col bg-white-1"
+      v-if="!open"
+      @click.stop="open = !open"
+    >
+      <div class="flex">
         <div
           class="aspect-square h-[48px] shrink-0 overflow-hidden rounded-md bg-background-2 dark:bg-background-dark-2"
         >
@@ -56,32 +50,6 @@ const {
             {{ currentTrack.meta?.artist }}
           </span>
         </div>
-      </div>
-      <div class="group relative select-none">
-        <div
-          v-if="isLoading"
-          role="status"
-          class="absolute -z-10 flex h-full w-full justify-center fill-background-4 align-middle dark:fill-background-dark-4"
-        >
-          <svg
-            aria-hidden="true"
-            class="flex aspect-square w-6 animate-spin justify-center align-middle"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              class="opacity-20"
-              fill="currentColor"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentColor"
-            />
-          </svg>
-          <span class="sr-only">Loading...</span>
-        </div>
         <button
           v-if="status === MediaPlayerStatus.Playing"
           @click.stop="pause()"
@@ -102,51 +70,109 @@ const {
           </span>
         </button>
       </div>
-      <div class="group select-none">
-        <button v-if="hasNext" @click.stop="next()">
+      <input
+        v-model="currentPosition"
+        type="range"
+        :min="0"
+        :max="currentTrackDuration"
+      />
+    </div>
+  </transition>
+
+  <transition
+    enter-active-class="transition-all duration-200 ease-out"
+    enter-from-class="opacity-0 translate-y-2"
+    leave-active-class="transition-all duration-200 ease-out"
+    leave-to-class="opacity-0 translate-y-2"
+  >
+    <div
+      v-if="open"
+      class="h-100 absolute bottom-5 right-5 flex flex-col bg-white-1"
+      @click.stop="open = !open"
+    >
+      <div class="flex justify-between">
+        <NuxtIcon name="icon.minify" filled class="text-2xl" />
+        <div>(LANGUAGE)</div>
+        <NuxtIcon name="options" filled class="text-2xl" />
+      </div>
+      <div
+        class="aspect-square h-[48px] shrink-0 overflow-hidden rounded-md bg-background-2 dark:bg-background-dark-2"
+      >
+        <ProtectedImage
+          v-if="currentTrack?.meta?.attachedPicture"
+          :src="currentTrack?.meta?.attachedPicture"
+        />
+      </div>
+      <div class="flex min-w-0 flex-col">
+        <h3
+          class="truncate text-center text-lg font-semibold leading-tight"
+          :title="currentTrack?.title || ''"
+        >
+          {{ currentTrack?.title }}
+        </h3>
+        <span
+          v-if="currentTrack?.meta?.artist"
+          class="truncate text-center text-base leading-snug text-label-2 dark:text-label-dark-2"
+          :title="currentTrack?.meta?.artist"
+        >
+          {{ currentTrack.meta?.artist }}
+        </span>
+      </div>
+      <input
+        v-model="currentPosition"
+        type="range"
+        :min="0"
+        :max="currentTrackDuration"
+      />
+      <div class="flex justify-between">
+        <span> <TimeDuration :duration="currentPosition"></TimeDuration></span>
+        <span>
+          <TimeDuration :duration="currentTrackDuration"></TimeDuration
+        ></span>
+      </div>
+      <div class="flex justify-evenly">
+        <NuxtIcon name="icon.rewind.large" filled class="text-2xl" />
+        <NuxtIcon name="icon.previous.track.large" filled class="text-2xl" />
+        <button
+          v-if="status === MediaPlayerStatus.Playing"
+          @click.stop="pause()"
+        >
           <span class="flex aspect-square w-12 justify-center align-middle">
             <NuxtIcon
-              name="icon.next.track.large"
+              name="icon.pause.large"
               class="text-3xl group-hover:text-4xl"
             />
           </span>
         </button>
+        <button
+          v-if="status !== MediaPlayerStatus.Playing"
+          @click.stop="play()"
+        >
+          <span class="flex aspect-square w-12 justify-center align-middle">
+            <NuxtIcon name="play" class="text-3xl group-hover:text-4xl" />
+          </span>
+        </button>
+        <NuxtIcon name="icon.next.track.large" filled class="text-2xl" />
+        <NuxtIcon name="icon.skip.large" filled class="text-2xl" />
       </div>
+      <hr />
+      <div class="flex justify-between">
+        <div>queue</div>
+        <div class="flex">
+          <NuxtIcon name="icon.shuffle" filled class="text-2xl" />
+          <NuxtIcon name="icon.repeat" filled class="text-2xl" />
+        </div>
+      </div>
+      <ul class="max-h-20 overflow-y-scroll">
+        <li
+          v-for="(item, i) in queue"
+          :key="i"
+          :class="queue.index === i ? 'bg-tint' : ''"
+          @click="queue.index = i"
+        >
+          {{ item.meta?.title || item.title }}
+        </li>
+      </ul>
     </div>
-    <input
-      v-model="currentPosition"
-      type="range"
-      :min="0"
-      :max="currentTrackDuration"
-    />
-    <TimeDuration :duration="currentPosition"></TimeDuration>
-    | <TimeDuration :duration="currentTrackDuration"></TimeDuration>
-    <div>
-      <button
-        v-if="queue.isShuffled"
-        class="rounded-full bg-background-4 px-2 py-2 text-background-3 hover:bg-background-3 hover:text-background-4 dark:bg-background-dark-4 dark:text-background-dark-3 dark:hover:bg-background-dark-3 dark:hover:text-background-dark-4"
-        @click.stop="queue.unshuffle()"
-      >
-        <NuxtIcon name="icon.shuffle" class="text-2xl" />
-      </button>
-      <button
-        v-else
-        class="rounded-full bg-background-3 px-2 py-2 text-background-4 hover:bg-background-4 hover:text-background-3 dark:bg-background-dark-3 dark:text-background-dark-4 dark:hover:bg-background-dark-4 dark:hover:text-background-dark-3"
-        @click.stop="queue.shuffle()"
-      >
-        <NuxtIcon name="icon.shuffle" class="text-2xl" />
-      </button>
-    </div>
-
-    <ul class="max-h-20 overflow-y-scroll">
-      <li
-        v-for="(item, i) in queue"
-        :key="i"
-        :class="queue.index === i ? 'bg-tint' : ''"
-        @click="queue.index = i"
-      >
-        {{ item.meta?.title || item.title }}
-      </li>
-    </ul>
-  </div>
+  </transition>
 </template>
