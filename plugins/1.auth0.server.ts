d51e9f2ea81f5753d34f92ec34ff275cec49a09b
Sigve Hansen <sighan12@gmail.com>
2023-10-11 01:49:27 +0200

    fix(auth): correctly type plugin

--- plugins/1.auth0.server.ts
+++ plugins/1.auth0.server.ts
@@ -1,17 +1,22 @@
-import { Auth0Plugin, AUTH0_INJECTION_KEY } from "@auth0/auth0-vue";
+import {
+  Auth0Plugin,
+  Auth0VueClient,
+  AUTH0_INJECTION_KEY,
+} from "@auth0/auth0-vue";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const auth0 = {
     getAccessTokenSilently: () => Promise.resolve(""),
     install: () => {},
   };
+
   auth0.install = () => {
     // eslint-disable-next-line no-param-reassign
     nuxtApp.vueApp.config.globalProperties.$auth0 =
-      auth0 as unknown as Auth0Plugin;
+      auth0 as unknown as Auth0VueClient;
     nuxtApp.vueApp.provide(
       AUTH0_INJECTION_KEY,
-      auth0 as unknown as Auth0Plugin
+      auth0 as unknown as Auth0VueClient
     );
   };
 
