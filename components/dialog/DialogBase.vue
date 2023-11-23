fcd909a00b5b964b9d671c1ba4f963b7a9c358a0
Sigve Hansen <sighan12@gmail.com>
2023-11-23 12:36:35 +0100

    fix(dialog): use correct background color on backdrop

--- components/dialog/DialogBase.vue
+++ components/dialog/DialogBase.vue
@@ -12,7 +12,7 @@ const emit = defineEmits<{
 <template>
   <Teleport to="body">
     <div v-if="show" class="relative z-30">
-      <div class="fixed inset-0 bg-background-dark-1 opacity-40"></div>
+      <div class="fixed inset-0 bg-background-4 opacity-40"></div>
 
       <div
         class="fixed inset-0 flex items-center justify-center content-center"
