97e07032669f52c2235648b70bde6f10d1c8bd9e
Simon Schick <simonsimcity@gmail.com>
2023-11-28 18:29:55 +0100

    Have menu of topbar overlaying the media player

--- components/AppToolbar.vue
+++ components/AppToolbar.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="sticky top-0 z-10 flex justify-between border-b border-label-separator bg-white-2 px-6 py-4 backdrop-blur-md dark:bg-black-2"
+    class="sticky top-0 z-20 flex justify-between border-b border-label-separator bg-white-2 px-6 py-4 backdrop-blur-md dark:bg-black-2"
   >
     <strong v-if="toolbarTitleStore().toolbarTitle" class="text-label-1">
       {{ toolbarTitleStore().toolbarTitle }}
