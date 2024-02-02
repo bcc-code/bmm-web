146112940d13732183440bf6c1f6b6b4b422bdb0
Karsten Küpper <karsten.kuepper@bcc.no>
2024-02-02 15:00:59 -0500

    remove typing

--- pages/featured.vue
+++ pages/featured.vue
@@ -1,12 +1,7 @@
 <script lang="ts" setup>
-import type { DocumentListIAllDocumentModels } from "@bcc-code/bmm-sdk-fetch";
-
 const { data: models, pending } = useFeaturedPlaylists();
 </script>
 
 <template>
-  <DocumentList
-    :list="models as DocumentListIAllDocumentModels"
-    :pending="pending"
-  ></DocumentList>
+  <DocumentList :list="models" :pending="pending"></DocumentList>
 </template>
