540396042bc3430b33e9c7a313f05e8cabdbd0c5
Simon Schick <simonsimcity@gmail.com>
2024-02-02 21:39:46 +0100

    Fix reloading of api requests if language is changed

--- utils/reactiveApi.ts
+++ utils/reactiveApi.ts
@@ -2,10 +2,7 @@ import type { AsyncData } from "nuxt/app";
 
 export default function reactiveApi<Data, Error>(data: AsyncData<Data, Error>) {
   const stopHandler = watch(
-    [
-      () => useNuxtApp().$i18n.locale,
-      () => contentLanguageStore().contentLanguages,
-    ],
+    [useNuxtApp().$i18n.locale, () => contentLanguageStore().contentLanguages],
     () => {
       data.refresh();
     },
