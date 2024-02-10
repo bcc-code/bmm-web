4a176ec6fd5f1a70a64fd094cd268744594fbb3d
Bastian Wissler <bastian.wissler@gmail.com>
2024-02-10 11:42:28 +0100

    :lipstick: animation / marque of title text

--- modules/figma2tailwind/convertToTailwind.ts
+++ modules/figma2tailwind/convertToTailwind.ts
@@ -24,6 +24,16 @@ const config: Partial<Config> = {
     extend: {  
       gridTemplateColumns: {
         tracklist: "3rem auto auto auto auto" 
+      },
+      animation: {
+        marquee: 'marquee 15s ease-in-out infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '85%': { transform: 'translateX(var(--animate-marquee-offset))' },
+          '100%': { transform: 'translateX(0%)' },
+        },
       }
     }
   },
