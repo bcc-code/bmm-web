8bb15647488db647d8130cfd6c7fe609435bdd59
Bastian Wissler <bastian.wissler@gmail.com>
2024-02-17 14:21:10 +0100

    Refactor language selection menu in TrackChangeLocale.vue

--- components/track/TrackChangeLocale.vue
+++ components/track/TrackChangeLocale.vue
@@ -1,19 +1,86 @@
 <script setup lang="ts">
+import { LanguageEnum } from "@bcc-code/bmm-sdk-fetch";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
 
+const expanded = ref(false);
 const { currentTrack } = useNuxtApp().$mediaPlayer;
+const { t } = useI18n();
 
+const changeLanguage = (lang: LanguageEnum) => {
+  expanded.value = false;
+  contentLanguageStore().selectedLanguage = lang;
+};
+
+const getSortedLanguages = () => {
+  const contentLangs = contentLanguageStore()
+    .contentLanguages as LanguageEnum[];
+  const tempLangs = currentTrack?.value?.languages || [];
+  const sortedLanguages = contentLangs.filter((lang) =>
+    tempLangs.includes(lang),
+  );
+  tempLangs.forEach((lang) => {
+    if (sortedLanguages.includes(lang)) {
+      return;
+    }
+    sortedLanguages.push(lang);
+  });
+
+  return sortedLanguages;
+};
+const firstFiveLanguages = () => getSortedLanguages().slice(0, 5);
+const allLanguages = () => getSortedLanguages();
 </script>
 <template>
-  <select
-    v-model="contentLanguageStore().selectedLanguage"
-    class="text-black-1 bg-background-1 dark:bg-background-3 dark:text-black-1 min-w-[100px] pl-3 py-2.5 shadow ring-1 ring-label-separator rounded-lg"
+  <Menu
+    as="div"
+    class="relative text-left flex flex-col justify-center"
+    @click.stop
   >
-    <option
-      v-for="(lang, i) in currentTrack?.languages"
-      :key="`Lang${i}`"
-      :value="lang"
+    <MenuButton
+      as="button"
+      :aria-label="t('track.a11y.options')"
+      class="rounded-full p-1 hover:bg-background-2 hover:text-label-1"
     >
-      {{ lang ? getLocalizedLanguageName(lang) : "" }}
-    </option>
-  </select>
+      {{
+        currentTrack?.language
+          ? getLocalizedLanguageName(currentTrack.language)
+          : ""
+      }}
+    </MenuButton>
+
+    <MenuItems
+      as="ul"
+      class="absolute right-0 top-10 z-30 w-52 rounded-xl p-1 shadow-md bg-background-3"
+    >
+      <div class="py-0">
+        <MenuItem
+          v-for="(lang, i) in expanded ? allLanguages() : firstFiveLanguages()"
+          :key="`Lang${i}`"
+          :visible="expanded || i < 5"
+          as="li"
+          class="block w-full cursor-pointer rounded-lg text-label-1 hover:bg-background-2 hover:text-black"
+        >
+          <button
+            class="flex w-full items-center justify-start gap-2 px-3 py-2"
+            @click="changeLanguage(lang)"
+          >
+            <span>{{ lang ? getLocalizedLanguageName(lang) : "" }}</span>
+          </button>
+        </MenuItem>
+        <MenuItem
+          v-if="!expanded"
+          as="li"
+          class="block w-full cursor-pointer rounded-lg text-label-1 hover:bg-background-2 hover:text-black"
+          @click.stop
+        >
+          <button
+            class="flex w-full items-center justify-start gap-2 px-3 py-2"
+            @click.stop="expanded = true"
+          >
+            {{ t("track.dropdown.show-all") }}
+          </button>
+        </MenuItem>
+      </div>
+    </MenuItems>
+  </Menu>
 </template>
