5b15ce9da66c12569247791c177b48b05fec88f2
Karsten Küpper <karsten.kuepper@bcc.no>
2024-02-23 14:08:34 -0500

    don't send age in requests anymore

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,9 @@
 import type { DiscoverGetRequest } from "@bcc-code/bmm-sdk-fetch";
 
 const { t } = useI18n();
-const userData = useNuxtApp().$userData;
 toolbarTitleStore().setReactiveToolbarTitle(() => t("nav.home"));
 
 const parameters: DiscoverGetRequest = {};
-if (userData.age) parameters.age = userData.age;
 const { data: models, pending } = useDiscover(parameters);
 </script>
 
