4b1bfae8045dc090e90208adc0749ece863bcfd4
Karsten Küpper <karsten.kuepper@bcc.no>
2024-02-26 07:39:23 -0500

    review fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,8 @@
 <script lang="ts" setup>
-import type { DiscoverGetRequest } from "@bcc-code/bmm-sdk-fetch";
-
 const { t } = useI18n();
 toolbarTitleStore().setReactiveToolbarTitle(() => t("nav.home"));
 
-const parameters: DiscoverGetRequest = {};
-const { data: models, pending } = useDiscover(parameters);
+const { data: models, pending } = useDiscover({});
 </script>
 
 <template>
