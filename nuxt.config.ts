a1d2e861e703c096a0a5ade62cf99f161a703fe0
Andreas Gangsø <andreasgangso@gmail.com>
2024-04-15 17:28:07 +0200

    add inter font weight 400 and 900

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -59,7 +59,7 @@ export default defineNuxtConfig({
           crossorigin: "anonymous",
         },
         {
-          href: "https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700;800&display=swap",
+          href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap",
           rel: "stylesheet",
         },
       ],
