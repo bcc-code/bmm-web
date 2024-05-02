47c53fc2ad69e3a072773cdba175ecdea7b9b473
Andreas Gangsø <andreasgangso@gmail.com>
2024-05-02 23:42:25 +0200

    lint fix

--- utils/scroll.ts
+++ utils/scroll.ts
@@ -1,19 +1,18 @@
-
 function getScrollParent(node: Element | Node | null): Element | null {
-    if (!node) {
-        return null;
-    }
-    if (!('scrollHeight' in node)) {
-        return null;
-    }
-    const isElement = node instanceof HTMLElement;
-    const overflowY = isElement && window.getComputedStyle(node).overflowY;
-    const isScrollable = overflowY !== "visible" && overflowY !== "hidden";
+  if (!node) {
+    return null;
+  }
+  if (!("scrollHeight" in node)) {
+    return null;
+  }
+  const isElement = node instanceof HTMLElement;
+  const overflowY = isElement && window.getComputedStyle(node).overflowY;
+  const isScrollable = overflowY !== "visible" && overflowY !== "hidden";
 
-    if (isScrollable && node.scrollHeight >= node.clientHeight) {
-        return node;
-    }
-    return getScrollParent(node.parentNode) || document.body;
+  if (isScrollable && node.scrollHeight >= node.clientHeight) {
+    return node;
+  }
+  return getScrollParent(node.parentNode) || document.body;
 }
 
 export default getScrollParent;
