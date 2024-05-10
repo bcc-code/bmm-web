cf4158d1f76a53eefc4724ede779727929560510
Karsten Küpper <karsten.kuepper@bcc.no>
2024-05-10 08:40:03 -0400

    copy solution for signing Windows .exe from bcc-code/bible-explorers (#419)
    * copy solution for signing Windows .exe from bcc-code/bible-explorers
    
    * (temporarily) enable signing on PR
    
    * add debug logs
    
    * fix checking for filename
    
    * escape file path
    
    * next try
    
    * remove debug logs
    
    * don't publish for PRs

---
+++ electron_sign_exe.js
@@ -0,0 +1,23 @@
+const { execSync } = require("child_process");
+
+module.exports = async function (configuration) {
+  if (!configuration.path.includes(configuration.name + " Setup")) {
+    return;
+  }
+
+  const filePath = configuration.path;
+  const directoryId = process.env.WINDOWS_DIRECTORY_ID;
+  const clientId = process.env.WINDOWS_CLIENT_ID;
+  const clientSecret = process.env.WINDOWS_CLIENT_SECRET;
+
+  try {
+    execSync(
+      `AzureSignTool.exe sign -du "https://bmm.bcc.media" -kvu "https://bccm-code-sign2.vault.azure.net" -kvt ${directoryId} -kvi ${clientId} -kvs ${clientSecret} -kvc "HSM-CS" -tr "http://timestamp.digicert.com" -v "${filePath}"`,
+      { stdio: "inherit" },
+    );
+    console.log(`Successfully signed ${filePath}`);
+  } catch (error) {
+    console.error(`Failed to sign ${filePath}`);
+    throw error;
+  }
+};
