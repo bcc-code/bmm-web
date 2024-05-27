714854e93211b2b7056954d25ac634c4d3e68b3a
Karsten Küpper <karsten.kuepper@bcc.no>
2024-05-27 15:54:19 -0400

    open player is on top of toolbar (e.g. when searching)

--- components/media-player/MediaPlayer.vue
+++ components/media-player/MediaPlayer.vue
@@ -22,7 +22,7 @@ defineShortcuts({
 <template>
   <div class="shrink-0" :class="{ 'w-[400px]': open }">
     <div
-      class="large-shadow group fixed bottom-5 right-5 flex w-[400px] flex-col overflow-hidden rounded-2xl bg-background-1 transition-all duration-700 ease-out-expo"
+      class="large-shadow group fixed bottom-5 right-5 z-30 flex w-[400px] flex-col overflow-hidden rounded-2xl bg-background-1 transition-all duration-700 ease-out-expo"
       :style="{
         height: open ? 'calc(100vh - 6rem)' : '4.5rem',
       }"
