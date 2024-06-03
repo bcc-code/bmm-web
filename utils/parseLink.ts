dd603f7afe54de8c913d0d5a339d802f590e5dd0
Karsten Küpper <karsten.kuepper@bcc.no>
2024-06-03 16:21:37 -0400

    don't modify external links

--- utils/parseLink.ts
+++ utils/parseLink.ts
@@ -1,5 +1,12 @@
 export default function parseLink(link: string) {
-  const url = new URL(link);
-  // We remove the hostname so that we use SPA links (without full page refresh)
-  return url.pathname + url.search + url.hash;
+  if (
+    link.startsWith(window.location.origin) ||
+    link.startsWith("https://bmm.bcc.media") ||
+    link.startsWith("https://bmm.brunstad.org")
+  ) {
+    const url = new URL(link);
+    // We remove the hostname so that we use SPA links (without full page refresh)
+    return url.pathname + url.search + url.hash;
+  }
+  return link;
 }
