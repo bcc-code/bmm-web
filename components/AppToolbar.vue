c486b73e95b0f3c429337b88ecd8b0bbeef275cc
Karsten Küpper <karsten.kuepper@bcc.no>
2024-06-12 13:05:57 -0400

    introduce hamburger menu

--- components/AppToolbar.vue
+++ components/AppToolbar.vue
@@ -8,16 +8,7 @@ const hasHistory = () => window.history.length > 1; // using a computed() doesn'
     style="-webkit-app-region: drag"
   >
     <div class="mx-5 flex flex-row">
-      <div class="flex flex-col justify-center pr-6">
-        <svg
-          class="fill-current block h-6 w-6"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <title>Mobile menu</title>
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-        </svg>
-      </div>
+      <div class="hamburger-teleport"></div>
       <div
         class="teleport ml-[-0.75rem]"
         style="-webkit-app-region: no-drag"
--- components/sidebar/SidebarElement.vue
+++ components/sidebar/SidebarElement.vue
@@ -5,60 +5,98 @@ const { data: collections } = usePrivatePlaylists();
 const runtimeConfig = useRuntimeConfig();
 const isElectron = runtimeConfig.public.systemName === "Electron";
 const isElectronOnMac = isElectron && runtimeConfig.public.isMac;
+const isMounted = ref<boolean>(false);
+onMounted(() => {
+  isMounted.value = true;
+});
+const hamburgerOpen = ref<boolean>(false);
 </script>
 
 <template>
-  <aside
-    v-if="false"
-    class="relative flex max-h-screen w-[300px] flex-none flex-col border-r border-label-separator bg-background-2"
-  >
-    <div
-      class="flex items-center p-3 px-6"
-      :class="{
-        'pb-1 pt-8': isElectronOnMac,
-      }"
-      style="-webkit-app-region: drag"
+  <div>
+    <Teleport
+      v-if="isMounted && !hamburgerOpen"
+      to="header .hamburger-teleport"
     >
-      <SiteLogo size="small" />
-      <span
-        class="mx-2 mt-1 inline-block rounded-xl bg-tint px-[5px] text-[13px] leading-5 text-black-1"
-        >Beta v{{ version }}</span
+      <div
+        class="flex h-full flex-col justify-center pr-6 md:hidden"
+        @click="hamburgerOpen = !hamburgerOpen"
       >
-    </div>
-    <div class="flex-grow overflow-y-auto">
-      <SidebarGroup>
-        <SidebarItem
-          :title="$t('nav.home')"
-          :link="{ name: 'index' }"
-          icon="nav.home"
-        />
-        <SidebarItem
-          :title="$t('nav.browse')"
-          :link="{ name: 'browse' }"
-          icon="nav.browse"
-        />
-        <SidebarItem
-          :title="$t('nav.search')"
-          :link="{ name: 'search-term' }"
-          icon="nav.search"
-        />
-      </SidebarGroup>
+        <svg
+          class="fill-current block h-6 w-6"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Mobile menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+        </svg>
+      </div>
+    </Teleport>
+    <aside
+      class="relative max-h-screen w-[300px] flex-none flex-col border-r border-label-separator bg-background-2"
+      :class="hamburgerOpen ? '' : 'hidden md:flex'"
+    >
+      <div
+        class="flex items-center p-3 px-6"
+        :class="{
+          'pb-1 pt-8': isElectronOnMac,
+        }"
+        style="-webkit-app-region: drag"
+      >
+        <div
+          class="ml-[-4px] mt-[3px] pr-3 md:hidden"
+          @click="hamburgerOpen = !hamburgerOpen"
+        >
+          <svg
+            class="fill-current block h-6 w-6"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Mobile menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+          </svg>
+        </div>
+        <SiteLogo size="small" />
+        <span
+          class="mx-2 mt-1 inline-block rounded-xl bg-tint px-[5px] text-[13px] leading-5 text-black-1"
+          >Beta v{{ version }}</span
+        >
+      </div>
+      <div class="flex-grow overflow-y-auto">
+        <SidebarGroup>
+          <SidebarItem
+            :title="$t('nav.home')"
+            :link="{ name: 'index' }"
+            icon="nav.home"
+          />
+          <SidebarItem
+            :title="$t('nav.browse')"
+            :link="{ name: 'browse' }"
+            icon="nav.browse"
+          />
+          <SidebarItem
+            :title="$t('nav.search')"
+            :link="{ name: 'search-term' }"
+            icon="nav.search"
+          />
+        </SidebarGroup>
 
-      <SidebarGroup :title="$t('sidebar.playlists.title')">
-        <SidebarItem
-          v-for="collection in collections"
-          :key="collection.id"
-          type="playlist"
-          :title="collection.name || ''"
-          :link="{
-            name: 'playlist-private-id',
-            params: { id: collection.id },
-          }"
-          icon="icon.category.playlist"
-        />
-        <SidebarAddPlaylist></SidebarAddPlaylist>
-      </SidebarGroup>
-      <SidebarDesktopPromo v-if="!isElectron" />
-    </div>
-  </aside>
+        <SidebarGroup :title="$t('sidebar.playlists.title')">
+          <SidebarItem
+            v-for="collection in collections"
+            :key="collection.id"
+            type="playlist"
+            :title="collection.name || ''"
+            :link="{
+              name: 'playlist-private-id',
+              params: { id: collection.id },
+            }"
+            icon="icon.category.playlist"
+          />
+          <SidebarAddPlaylist></SidebarAddPlaylist>
+        </SidebarGroup>
+        <SidebarDesktopPromo v-if="!isElectron" />
+      </div>
+    </aside>
+  </div>
 </template>
