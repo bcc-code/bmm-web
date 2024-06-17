56fa93c8a0bf5c58e2ddb0c1013fccf082949d3d
Karsten Küpper <karsten.kuepper@bcc.no>
2024-06-17 14:00:55 -0400

    make sidebar scrollable again (#452)

--- components/sidebar/SidebarElement.vue
+++ components/sidebar/SidebarElement.vue
@@ -31,59 +31,61 @@ const hamburgerOpen = ref<boolean>(false);
     class="fixed bottom-0 top-0 z-50 max-h-screen w-[300px] flex-none flex-col border-r border-label-separator bg-background-2 shadow-2xl transition-all duration-200 ease-in-out md:relative"
     :class="hamburgerOpen ? 'left-0 ' : '-left-full md:left-0 md:shadow-none'"
   >
-    <div
-      class="flex items-center p-3 px-6"
-      :class="{
-        'pb-1 pt-8': isElectronOnMac,
-      }"
-      style="-webkit-app-region: drag"
-    >
+    <div class="flex h-full flex-col">
       <div
-        class="ml-[-4px] mt-[3px] pr-3 md:hidden"
-        @click="hamburgerOpen = !hamburgerOpen"
+        class="flex items-center p-3 px-6"
+        :class="{
+          'pb-1 pt-8': isElectronOnMac,
+        }"
+        style="-webkit-app-region: drag"
       >
-        <NuxtIcon name="icon.hamburger" class="fill-label-1 text-2xl" />
+        <div
+          class="ml-[-4px] mt-[3px] pr-3 md:hidden"
+          @click="hamburgerOpen = !hamburgerOpen"
+        >
+          <NuxtIcon name="icon.hamburger" class="fill-label-1 text-2xl" />
+        </div>
+        <SiteLogo size="small" />
+        <span
+          class="mx-2 mt-1 inline-block rounded-xl bg-tint px-[5px] text-[13px] leading-5 text-black-1"
+          >Beta v{{ version }}</span
+        >
       </div>
-      <SiteLogo size="small" />
-      <span
-        class="mx-2 mt-1 inline-block rounded-xl bg-tint px-[5px] text-[13px] leading-5 text-black-1"
-        >Beta v{{ version }}</span
-      >
-    </div>
-    <div class="flex-grow overflow-y-auto">
-      <SidebarGroup>
-        <SidebarItem
-          :title="$t('nav.home')"
-          :link="{ name: 'index' }"
-          icon="nav.home"
-        />
-        <SidebarItem
-          :title="$t('nav.browse')"
-          :link="{ name: 'browse' }"
-          icon="nav.browse"
-        />
-        <SidebarItem
-          :title="$t('nav.search')"
-          :link="{ name: 'search-term' }"
-          icon="nav.search"
-        />
-      </SidebarGroup>
+      <div class="flex-grow overflow-y-auto">
+        <SidebarGroup>
+          <SidebarItem
+            :title="$t('nav.home')"
+            :link="{ name: 'index' }"
+            icon="nav.home"
+          />
+          <SidebarItem
+            :title="$t('nav.browse')"
+            :link="{ name: 'browse' }"
+            icon="nav.browse"
+          />
+          <SidebarItem
+            :title="$t('nav.search')"
+            :link="{ name: 'search-term' }"
+            icon="nav.search"
+          />
+        </SidebarGroup>
 
-      <SidebarGroup :title="$t('sidebar.playlists.title')">
-        <SidebarItem
-          v-for="collection in collections"
-          :key="collection.id"
-          type="playlist"
-          :title="collection.name || ''"
-          :link="{
-            name: 'playlist-private-id',
-            params: { id: collection.id },
-          }"
-          icon="icon.category.playlist"
-        />
-        <SidebarAddPlaylist></SidebarAddPlaylist>
-      </SidebarGroup>
-      <SidebarDesktopPromo v-if="!isElectron" />
+        <SidebarGroup :title="$t('sidebar.playlists.title')">
+          <SidebarItem
+            v-for="collection in collections"
+            :key="collection.id"
+            type="playlist"
+            :title="collection.name || ''"
+            :link="{
+              name: 'playlist-private-id',
+              params: { id: collection.id },
+            }"
+            icon="icon.category.playlist"
+          />
+          <SidebarAddPlaylist></SidebarAddPlaylist>
+        </SidebarGroup>
+        <SidebarDesktopPromo v-if="!isElectron" />
+      </div>
     </div>
   </aside>
 </template>
