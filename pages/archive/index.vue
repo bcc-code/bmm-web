d243f116d780d154bc9656a9559e20cc0c2379f8
Karsten Küpper <karsten.kuepper@bcc.no>
2024-06-18 14:49:45 -0400

    implement archive

---
+++ pages/archive/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const { t } = useI18n();
+toolbarTitleStore().setReactiveToolbarTitle(() => t("nav.archive"));
+
+const { data: years } = useYearList();
+</script>
+
+<template>
+  <div>
+    <div class="h-3"></div>
+    <div class="columns-2 md:columns-3 xl:columns-4 2xl:columns-5">
+      <NuxtLink
+        v-for="(year, i) in years?.toReversed()"
+        :key="`year${i}`"
+        :to="{ name: 'archive-year', params: { year: year.year } }"
+        class="mx-8 flex flex-row items-center justify-center rounded-xl hover:bg-background-2"
+      >
+        <div class="type-subtitle-1 px-2 py-2">{{ year.year }}</div>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
