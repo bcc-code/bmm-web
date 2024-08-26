92e1bb2ecbc1191ba1a0c84686cd44202ff8f0d3
Sigve Hansen <sighan12@gmail.com>
2024-08-26 11:55:19 +0200

    feat: animate entering player when first track is started

--- components/media-player/MediaPlayer.vue
+++ components/media-player/MediaPlayer.vue
@@ -21,13 +21,14 @@ defineShortcuts({
 
 <template>
   <div
-    class="shrink-0 transition-all duration-700 ease-out-expo"
+    class="shrink-0 transition-[width] duration-700 ease-out-expo"
     :class="{ 'w-[400px]': open, 'w-0': !open }"
   >
     <div
-      class="large-shadow group fixed bottom-1 right-0 z-30 flex w-[400px] max-w-full flex-col overflow-hidden rounded-2xl bg-background-1 transition-all duration-700 ease-out-expo sm:bottom-5 sm:right-5"
+      class="large-shadow group fixed bottom-1 right-0 z-30 flex w-[400px] max-w-full animate-[player-enter_200ms] flex-col overflow-hidden rounded-2xl bg-background-1 transition-all duration-700 ease-out-expo sm:bottom-5 sm:right-5"
       :style="{
         height: open ? 'calc(100vh - 6rem)' : '4.5rem',
+        'animation-timing-function': 'cubic-bezier(0.19, 1, 0.22, 1);',
       }"
     >
       <MediaPlayerClosed v-if="!open" @click.stop="open = !open" />
