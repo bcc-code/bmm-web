a9b36835eaf6688523dcdd7f1adf064bd401b1ff
Sigve Hansen <sighan12@gmail.com>
2024-10-24 14:04:29 +0200

    style: lint

--- middleware/frakaareDashboardViewer.ts
+++ middleware/frakaareDashboardViewer.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line consistent-return
 export default defineNuxtRouteMiddleware(async () => {
-	const { data: currentUser } = await useCurrentUser();
-	if (!isFraKaareDashboardViewer(currentUser.value)) return navigateTo("/");
+  const { data: currentUser } = await useCurrentUser();
+  if (!isFraKaareDashboardViewer(currentUser.value)) return navigateTo("/");
 });
