322d2f42f79ee4f932f155e4f25e16d4fb0ea9e9
Karsten Küpper <karsten.kuepper@bcc.no>
2025-01-14 12:08:29 -0500

    delete properties with "" so that fallback language is used

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,3 +1,5 @@
+import type { LocaleMessage } from "@intlify/core";
+import type { VueMessageType } from "vue-i18n";
 import de from "./locales/de.json";
 import en from "./locales/en.json";
 import nb from "./locales/nb.json";
@@ -15,41 +17,48 @@ import pt from "./locales/pt.json";
 import ro from "./locales/ro.json";
 import ru from "./locales/ru.json";
 
+function cleanupAny(existing: any, fallback: any): any {
+  Object.keys(existing).forEach((key) => {
+    if (typeof existing[key] === "object") {
+      existing[key] = cleanupAny(existing[key], fallback[key]);
+    }
+    if (existing[key] === "") {
+      existing[key] = fallback[key];
+    }
+  });
+  return existing;
+}
+
+function cleanup(
+  existing: LocaleMessage<VueMessageType>,
+  fallback: LocaleMessage<VueMessageType>,
+): LocaleMessage<VueMessageType> {
+  Object.keys(existing).forEach((key) => {
+    if (typeof existing[key] === "object") {
+      existing[key] = cleanupAny(existing[key], fallback[key]);
+    }
+  });
+  return existing;
+}
+
 export default defineI18nConfig(() => ({
   legacy: false,
   messages: {
-    nb,
+    nb: cleanup(nb, en),
     en,
-    de,
-    nl,
-    fr,
-    sl,
-    tr,
-    ta,
-    da,
-    es,
-    et,
-    fi,
-    hu,
-    pt,
-    ro,
-    ru,
-  },
-  fallbackRootWithEmptyString: true,
-  postTranslation: (value, path) => {
-    if (value === "") {
-      const parts = path.split(".");
-      let message = en;
-      return "empty translation";
-      for (const key of parts) {
-        if (key in message) {
-          message = message[key];
-        } else {
-          return value;
-        }
-      }
-      return message;
-    }
-    return value;
+    de: cleanup(de, en),
+    nl: cleanup(nl, en),
+    fr: cleanup(fr, en),
+    sl: cleanup(sl, en),
+    tr: cleanup(tr, en),
+    ta: cleanup(ta, en),
+    da: cleanup(da, en),
+    es: cleanup(es, en),
+    et: cleanup(et, en),
+    fi: cleanup(fi, en),
+    hu: cleanup(hu, en),
+    pt: cleanup(pt, en),
+    ro: cleanup(ro, en),
+    ru: cleanup(ru, en),
   },
 }));
