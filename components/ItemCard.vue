10d66413be4a9944cde227fa38ff23c95076355d
Sigve Hansen <sighan12@gmail.com>
2025-03-20 10:23:44 +0100

    feat: add title beneath cover in item card

--- components/ItemCard.vue
+++ components/ItemCard.vue
@@ -13,5 +13,8 @@ defineProps<{
 <template>
   <div :title="item.title || ''">
     <CoverImage :src="item.cover" class="w-full rounded-2xl" />
+    <span class="type-paragraph-3 mt-2 line-clamp-2 leading-snug text-label-1">
+      {{ item.title }}
+    </span>
   </div>
 </template>
