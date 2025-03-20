daca12ee22fcaa6fd6b45426489d21e6ed41c965
Sigve Hansen <sighan12@gmail.com>
2025-03-20 11:44:46 +0100

    fix: center album title + max one line

--- components/ItemCard.vue
+++ components/ItemCard.vue
@@ -13,7 +13,9 @@ defineProps<{
 <template>
   <div :title="item.title || ''">
     <CoverImage :src="item.cover" class="w-full rounded-2xl" />
-    <span class="type-paragraph-3 mt-2 line-clamp-2 leading-snug text-label-1">
+    <span
+      class="type-paragraph-3 mt-2 line-clamp-1 w-full text-center leading-snug text-label-1"
+    >
       {{ item.title }}
     </span>
   </div>
