e6415098b745da50ad8a1899881fb1d964d1e544
Adelin Niculae <43118261+adelinn@users.noreply.github.com>
2025-04-10 10:36:29 +0300

    Update login.cy.ts

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -3,14 +3,15 @@ describe("template spec", () => {
     cy.visit("/");
 
     cy.origin("login.bcc.no", () => {
-      cy.get("form.auth0-lock-widget input[name=email]").type(
+      cy.get("main._widget input[name=username]").type(
         Cypress.env("LOGIN_USERNAME"),
       );
-      cy.get("form.auth0-lock-widget input[name=password]").type(
+      cy.get("main._widget button[name=action]").click();
+      cy.get("main._widget input[name=password]").type(
         Cypress.env("LOGIN_PASSWORD"),
         { log: false },
       );
-      cy.get("form.auth0-lock-widget button[name=submit]").click();
+      cy.get("main._widget button[name=action]").click();
     });
 
     cy.url().should("match", new RegExp(`^${Cypress.config().baseUrl}/`));
